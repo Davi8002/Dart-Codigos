edef9375eba5624d86615882fcbc0b7e72f2384f
Davi <164496370+Davi8002@users.noreply.github.com>
2025-02-17 21:09:04 -0300

    Update codigo1.dart

--- codigo1.dart
+++ codigo1.dart
@@ -9,12 +9,12 @@ void main() {
   bool teste = true;
   print(teste);
 
-  List<int> numeros_primos = [2, 3, 5, 7, 11];
-  print(numeros_primos);
-  print(numeros_primos[0]);
+  List<int> numerosPrimos = [2, 3, 5, 7, 11];
+  print(numerosPrimos);
+  print(numerosPrimos[0]);
   
   
- List<String> nome_sobrenome = ["Davi", "Ribeiro"];
-  print("${nome_sobrenome[0]} ${nome_sobrenome[1]}");
+ List<String> nomeSobrenome = ["Davi", "Ribeiro"];
+  print("${nomeSobrenome[0]} ${nomeSobrenome[1]}");
    
   }
