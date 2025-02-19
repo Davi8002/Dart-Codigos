c3cfb5601feffd2c6d495fe282790fc2fd766e19
Davi <164496370+Davi8002@users.noreply.github.com>
2025-02-19 20:08:17 -0300

    Update codigo2.dart

--- codigo2.dart
+++ codigo2.dart
@@ -1,1 +1,30 @@
+void main() {
+  //IF e SWITCH
 
+  bool mueheheh = true;
+
+  if (!mueheheh) {
+    print("no mueheheh");
+  } else {
+    print("yes mueheheh");
+  }
+
+  int number = 5;
+
+  switch (number) {
+    case 1:
+      print("UM");
+      break;
+    case 3:
+      print("TRES");
+      break;
+    case 5:
+      print("CINCO");
+      break;
+    default:
+      print("seinao");
+  }
+  
+  //LOOP
+  
+}
